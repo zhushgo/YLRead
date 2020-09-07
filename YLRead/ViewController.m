0b008d4394ca210c03136ebeac4c02abb07b8007
SuMoLi <774792381@qq.com>
2020-09-07 09:29:06 +0800

    后续

--- YLRead/ViewController.m
+++ YLRead/ViewController.m
@@ -43,7 +43,7 @@
     
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         //[BiQuGeParser getBookAllStringByBookID:@"53605"];
-//         [LingDianParser getBookAllStringByBookID:@"4626"];
+//         [LingDianParser getBookAllStringByBookID:@"410820"];
         NSLog(@"%@", [NSString stringWithFormat:@"%@/Documents/",NSHomeDirectory()]);
     });
 
