5131b73c96d1001d21e8a785a21096893264a59e
SuMoLi <774792381@qq.com>
2020-11-05 17:07:59 +0800

    init

--- YLReader-OC/YLRead/ViewController.m
+++ YLReader-OC/YLRead/ViewController.m
@@ -39,7 +39,7 @@
     
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         //[BiQuGeParser getBookAllStringByBookID:@"53605"];
-         //[LingDianParser getBookAllStringByBookID:@"17108"];
+        //[LingDianParser getBookAllStringByBookID:@"60231"];
         NSLog(@"%@", [NSString stringWithFormat:@"%@/Documents/",NSHomeDirectory()]);
     });
 }
